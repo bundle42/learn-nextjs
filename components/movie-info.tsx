73d61a9bdfac0020a2687af2c067b194b281bc43
bundle42 <zzz9785@naver.com>
2025-11-21 21:28:01 +0900

    v 1.0

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,13 +1,34 @@
+import potato from "../styles/movie-info.module.css";
 import { API_URL } from "../app/(home)/page";
 
-async function getMovie(id: string) {
-  console.log(`Fetching movies: ${Date.now()}`);
-  //await new Promise((resolve) => setTimeout(resolve, 1000));
+export async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`, { cache: "force-cache" });
   return response.json();
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={potato.container}>
+      <img
+        src={movie.poster_path}
+        className={potato.poster}
+        alt={movie.title}
+      />
+      <div className={potato.info}>
+        <h1 className={potato.title}>{movie.title}</h1>
+        <h3>★{movie.vote_average.toFixed(2)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target="_blank">
+          Homepage &rarr;
+        </a>
+        <h3>Profit: ${movie.revenue - movie.budget}</h3>
+        <img
+          src={movie.production_companies[0].logo_path}
+          className={potato.company}
+          alt={movie.title}
+        />
+      </div>
+    </div>
+  );
 }
